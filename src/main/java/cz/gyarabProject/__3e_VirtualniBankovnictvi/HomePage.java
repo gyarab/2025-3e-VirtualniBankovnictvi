87357fbff74437582bd3319ec4da8801d706483d
Lukas <Lukas@MEDVED>
2025-11-17 13:04:41 +0100

    test

--- src/main/java/cz/gyarabProject/__3e_VirtualniBankovnictvi/HomePage.java
+++ src/main/java/cz/gyarabProject/__3e_VirtualniBankovnictvi/HomePage.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomePage {
 
-    @GetMapping("/")
+    @GetMapping(value="/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
